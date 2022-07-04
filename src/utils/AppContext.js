bc758b98bee1f2e8ba5f884fd36e0646f166b1df
Diego Murillo <diegomurilloferrer@gmail.com>
2022-07-04 17:12:41 -0500

    Persist the number of posts in local storage

--- src/utils/AppContext.js
+++ src/utils/AppContext.js
@@ -1,12 +1,12 @@
 /* eslint-disable react/prop-types */
-import React, { createContext, useMemo, useState } from "react";
+import React, { createContext, useMemo } from "react";
 // eslint-disable-next-line
 import { usePersistentState } from "react-persistent-state";
 
 export const AppContext = createContext();
 
 export default function AppContextProvider({ children }) {
-    const [numberOfPost, setNumberOfPost] = useState(6);
+    const [numberOfPost, setNumberOfPost] = usePersistentState(6);
     const [favoriteJobsList, setFavoriteJobsList] = usePersistentState([]);
     return (
         <AppContext.Provider
