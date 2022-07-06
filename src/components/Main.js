cf0155a73d936cfb280219cdc9e2dafc5949e2dd
Diego Murillo <diegomurilloferrer@gmail.com>
2022-07-06 17:03:52 -0500

    Fix scroll position bug

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,21 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+/* eslint-disable no-console */
+import React, { useEffect, useLayoutEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import GeneralJobs from "./GeneralJobs";
 import FavoriteJobs from "./FavoriteJobs";
 import JobDetails from "./JobDetails";
 
 export default function Main() {
+    const location = useLocation();
+
+    useLayoutEffect(() => {
+        window.scrollTo(0, 0);
+    }, [location.pathname]);
+
+    useEffect(() => {
+        console.log(location);
+    }, [location]);
+
     return (
         <Routes>
             <Route path="/" element={<GeneralJobs />} />
