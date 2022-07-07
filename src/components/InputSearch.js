fbe5a56f0ffad2a321b01bb4b052182c58db6457
Diego Murillo <diegomurilloferrer@gmail.com>
2022-07-07 14:16:35 -0500

    Make inputs of InputSearch controlled

--- src/components/InputSearch.js
+++ src/components/InputSearch.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import {
     Box,
     Grid,
@@ -10,14 +11,41 @@ import {
     Select,
     InputLabel,
 } from "@mui/material";
-import React from "react";
-// import CenterContainerWrapper from "./CenterContainerWrapper";
+import React, { useEffect, useState } from "react";
 
 export default function InputSearch() {
+    const [techKeywords, setTechKeywords] = useState("");
+    const [experience, setExperience] = useState("");
+    const [companyType, setCompanyType] = useState("");
+    const [hours, setHours] = useState("");
+
+    const handleChangeTechKeywords = (event) => {
+        setTechKeywords(event.target.value);
+    };
+
+    const handleChangeExperience = (event) => {
+        setExperience(event.target.value);
+    };
+
+    const handleCompanyType = (event) => {
+        setCompanyType(event.target.value);
+    };
+
+    const handleHours = (event) => {
+        setHours(event.target.value);
+    };
+
+    useEffect(() => {
+        console.log("Tech keyword:", techKeywords);
+        console.log("Experience:", experience);
+        console.log("Company type:", companyType);
+        console.log("Hours:", hours);
+    });
+
     return (
         <Box sx={{ mt: 12 }}>
-            <Container maxWidth="sm" align="center" sx={{ px: 5 }}>
-                <Typography variant="h4" color="initial">
+            <Container maxWidth="sm" sx={{ px: 5 }}>
+                <Typography variant="h4" color="initial" align="center">
                     Hey! What are you looking for today?
                 </Typography>
                 <Typography
@@ -34,6 +62,8 @@ export default function InputSearch() {
                     label="E.g: Front-end, Back-end..."
                     sx={{ mt: 4 }}
                     fullWidth
+                    value={techKeywords}
+                    onChange={handleChangeTechKeywords}
                 />
                 <Typography
                     variant="h6"
@@ -52,11 +82,14 @@ export default function InputSearch() {
                             <Select
                                 labelId="experience-for-jobs"
                                 label="Experience"
+                                value={experience}
+                                onChange={handleChangeExperience}
                             >
-                                <MenuItem>Junior</MenuItem>
-                                <MenuItem>Regular</MenuItem>
-                                <MenuItem>Senior</MenuItem>
-                                <MenuItem>Lead</MenuItem>
+                                <MenuItem value="Junior">Junior</MenuItem>
+                                <MenuItem value="Regular">Regular</MenuItem>
+                                <MenuItem value="Senior">Senior</MenuItem>
+                                <MenuItem value="Lead">Lead</MenuItem>
+                                <MenuItem value="All">All</MenuItem>
                             </Select>
                         </FormControl>
                     </Grid>
@@ -68,20 +101,31 @@ export default function InputSearch() {
                             <Select
                                 labelId="company-type-for-jobs"
                                 label="Company Type"
+                                value={companyType}
+                                onChange={handleCompanyType}
                             >
-                                <MenuItem>Startup</MenuItem>
-                                <MenuItem>Product</MenuItem>
-                                <MenuItem>Services</MenuItem>
+                                <MenuItem value="Startup">Startup</MenuItem>
+                                <MenuItem value="Product">Product</MenuItem>
+                                <MenuItem value="Services">Services</MenuItem>
+                                <MenuItem value="All">All</MenuItem>
                             </Select>
                         </FormControl>
                     </Grid>
                     <Grid item xs={12} md={4}>
                         <FormControl fullWidth>
                             <InputLabel id="hours-for-jobs">Hours</InputLabel>
-                            <Select labelId="hours-for-jobs" label="Hours">
-                                <MenuItem>Full-Time</MenuItem>
-                                <MenuItem>Part-Time</MenuItem>
-                                <MenuItem>Contractor</MenuItem>
+                            <Select
+                                labelId="hours-for-jobs"
+                                label="Hours"
+                                value={hours}
+                                onChange={handleHours}
+                            >
+                                <MenuItem value="Full-Time">Full-Time</MenuItem>
+                                <MenuItem value="Part-Time">Part-Time</MenuItem>
+                                <MenuItem value="Contractor">
+                                    Contractor
+                                </MenuItem>
+                                <MenuItem value="All">All</MenuItem>
                             </Select>
                         </FormControl>
                     </Grid>
