f258432be30d3afe644bb6cc9247d27452bb4de3
Diego Murillo <diegomurilloferrer@gmail.com>
2022-08-18 11:41:43 -0500

    Update hook for data fetching

--- src/utils/useFetchData.js
+++ src/utils/useFetchData.js
@@ -2,7 +2,13 @@ import { useQuery } from "react-query";
 
 export default function useFetchData() {
     return useQuery("JobPosting", async () => {
-        const response = await fetch("https://devitjobs.us/api/jobs");
+        const response = await fetch("https://devitjobs.us/api/jobs", {
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+            },
+        });
         if (!response.ok) {
             throw new Error("Oops, an error ocurred fetching the data");
         }
