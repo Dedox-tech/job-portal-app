377a7f9bb40e5fa20799d54c3c4736a2c1193a9c
Diego Murillo <diegomurilloferrer@gmail.com>
2022-08-18 13:54:22 -0500

    Add proxy for client request to third party server

--- src/utils/useFetchData.js
+++ src/utils/useFetchData.js
@@ -2,13 +2,15 @@ import { useQuery } from "react-query";
 
 export default function useFetchData() {
     return useQuery("JobPosting", async () => {
-        const response = await fetch("https://devitjobs.us/api/jobs", {
-            mode: "cors",
-            headers: {
-                "Content-Type": "application/json",
-                "Access-Control-Allow-Origin": "*",
-            },
-        });
+        const response = await fetch(
+            "https://obscure-peak-82556.herokuapp.com/https://devitjobs.us/api/jobs",
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-Requested-With": "XMLHttpRequest",
+                },
+            }
+        );
         if (!response.ok) {
             throw new Error("Oops, an error ocurred fetching the data");
         }
